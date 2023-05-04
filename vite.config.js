2d4280823d54e0b0558108c5f31f0bab3d851d47
phathak <phathak2002@hotmail.com>
2023-05-04 14:03:38 -0400

    removed one folder level

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/nimby_bingo/frontend/",
+  base: "/nimby_bingo/",
   plugins: [react()],
 });
