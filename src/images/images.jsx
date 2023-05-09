f68aa5868eb8d2e1af2a5f420add7ab1aa378fdc
phathak <phathak2002@hotmail.com>
2023-05-09 05:50:07 -0400

    fix background image

--- src/images/images.jsx
+++
@@ -1,4 +0,0 @@
-const IMAGES = {
-  image1: new URL("./pexels-alex-blokstra-1085653.jpg", import.meta.url).href,
-};
-export default IMAGES;
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter } from "react-router-dom";
 import { SubjectProvider } from "./contexts/subject_context";
+import {ScoreProvider} from "./contexts/score_context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <SubjectProvider>
-        <App />
-      </SubjectProvider>
+      <ScoreProvider>
+          <SubjectProvider>
+            <App />
+          </SubjectProvider>
+      </ScoreProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
